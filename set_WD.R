064ac23fba23525e8c9b16abd98f1318ae1f80d4
Wayne <sansugui@gmail.com>
2018-04-08 13:39:20 -0700

    change wd

--- set_WD.R
+++ set_WD.R
@@ -2,8 +2,4 @@
 
 # Change this for your own setup!!!
 
-# CC: 
-# setwd("C:/Users/Chris Castillo/Data Science/Projects/House Prices - Kaggle Competition/HousingPrices/")
-
-# WW:
-setwd("~/HousingPrices/")
+# setwd("YOUR_USER_PATH")
